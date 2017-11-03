0848c89ebc83e659a49fb1dcaf51ed7b10bad572
Dmitry A. Ilyashevich <dmitry.ilyashevich@gmail.com>
2017-11-03 22:59:29 +0300

    Added boolean type scopes

--- lib/type_scopes.rb
+++ lib/type_scopes.rb
@@ -1,11 +1,13 @@
 require "string_scopes"
 require "numeric_scopes"
 require "timestamp_scopes"
+require "boolean_scopes"
 
 module TypeScopes
   def self.included(model)
     model.include(StringScopes)
     model.include(NumericScopes)
     model.include(TimestampScopes)
+    model.include(BooleanScopes)
   end
 end
