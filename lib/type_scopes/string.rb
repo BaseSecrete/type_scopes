d04466766ad7f70f08666223972a2d5b233c1b80
Alexis Bernard <alexis@basesecrete.com>
2024-02-22 16:07:36 +0100

    Use Rails sanitize_sql_like instead of custom method

--- lib/type_scopes/string.rb
+++ lib/type_scopes/string.rb
@@ -3,12 +3,6 @@ class TypeScopes::String < TypeScopes
     ["character", "text", "varchar"].freeze
   end
 
-  def self.escape(string)
-    string = string.gsub("%".freeze, "[%]".freeze)
-    string.gsub!("_".freeze, "[_]".freeze)
-    string
-  end
-
   def self.inject_for_column(model, name)
     column = model.arel_table[name]
     append_scope(model, :"#{name}_like", lambda { |str, sensitive: true| where(column.matches(str, nil, sensitive)) })
@@ -17,31 +11,31 @@ class TypeScopes::String < TypeScopes
     append_scope(model, :"#{name}_not_ilike", lambda { |str| where(column.does_not_match(str)) })
 
     append_scope(model, :"#{name}_contains", lambda { |str, sensitive: true|
-      send("#{name}_like", "%#{TypeScopes::String.escape(str)}%", sensitive: sensitive)
+      send("#{name}_like", "%#{sanitize_sql_like(str)}%", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_does_not_contain", lambda { |str, sensitive: true|
-      send("#{name}_not_like", "%#{TypeScopes::String.escape(str)}%", sensitive: sensitive)
+      send("#{name}_not_like", "%#{sanitize_sql_like(str)}%", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_does_not_contain", lambda { |str, sensitive: true|
-      send("#{name}_like", "%#{TypeScopes::String.escape(str)}%", sensitive: sensitive)
+      send("#{name}_like", "%#{sanitize_sql_like(str)}%", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_starts_with", lambda { |str, sensitive: true|
-      send("#{name}_like", "#{TypeScopes::String.escape(str)}%", sensitive: sensitive)
+      send("#{name}_like", "#{sanitize_sql_like(str)}%", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_does_not_start_with", lambda { |str, sensitive: true|
-      send("#{name}_not_like", "#{TypeScopes::String.escape(str)}%", sensitive: sensitive)
+      send("#{name}_not_like", "#{sanitize_sql_like(str)}%", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_ends_with", lambda { |str, sensitive: true|
-      send("#{name}_like", "%#{TypeScopes::String.escape(str)}", sensitive: sensitive)
+      send("#{name}_like", "%#{sanitize_sql_like(str)}", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_does_not_end_with", lambda { |str, sensitive: true|
-      send("#{name}_not_like", "%#{TypeScopes::String.escape(str)}", sensitive: sensitive)
+      send("#{name}_not_like", "%#{sanitize_sql_like(str)}", sensitive: sensitive)
     })
 
     append_scope(model, :"#{name}_matches", lambda { |str, sensitive: true| where(column.matches_regexp(str, sensitive)) })
